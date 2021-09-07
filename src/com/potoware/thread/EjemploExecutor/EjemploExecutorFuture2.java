731a7175a1fa4937c9bb4488e1808675c1a702ea
Alejandro Potosi <alejandropotosi@outlook.com>
2021-09-06 21:57:15 -0500

    Fixed Thread Pool - Executor

---
+++ src/com/potoware/thread/EjemploExecutor/EjemploExecutorFuture2.java
@@ -0,0 +1,47 @@
+package com.potoware.thread.EjemploExecutor;
+
+import java.util.concurrent.*;
+
+public class EjemploExecutorFuture2 {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException, TimeoutException {
+
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+
+        Callable<String> tarea= ()->{
+            System.out.println("inicio de la tarea");
+            try {
+                System.out.println("Nombre del Thread " + Thread.currentThread().getName());
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                e.printStackTrace();
+            }
+            System.out.println("Finaliza la tarea...");
+            return "Algun resultado ";
+        };
+
+        Callable<Integer> tarea2 =()->{
+            TimeUnit.SECONDS.sleep(3);
+            return 10;
+        };
+
+        Future<String>resultadoFuturo = executor.submit(tarea);
+        Future<String>resultadoFuturo2 = executor.submit(tarea);
+        Future<Integer> resultadoFuturo3 = executor.submit(tarea2);
+        executor.shutdown();
+        System.out.println("Continuando con la ejecucion del main 1");
+        System.out.println(resultadoFuturo.isDone());
+        while (!(resultadoFuturo.isDone()&&resultadoFuturo2.isDone()&&resultadoFuturo3.isDone())){
+            System.out.println(String.format("resultado 1: %s -resultado 2: %s - resultado 3: %s",
+                    resultadoFuturo.isDone()? "finalizo":"en proceso",
+                    resultadoFuturo2.isDone()? "finalizo":"en proceso",
+                    resultadoFuturo3.isDone()? "finalizo":"en proceso"));
+            TimeUnit.MILLISECONDS.sleep(1500);
+        }
+
+        System.out.println(resultadoFuturo.get());
+        System.out.println(resultadoFuturo.isDone());
+
+    }
+}
